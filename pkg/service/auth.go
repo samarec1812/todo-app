57072d812e42dc2ee4303455fc57e2e668120fc9
samarec1812 <samarec1812@mail.ru>
2022-03-01 15:49:46 +0300

    todo-app base app

---
+++ pkg/service/auth.go
@@ -0,0 +1,78 @@
+package service
+
+import (
+	"crypto/sha1"
+	"errors"
+	"fmt"
+	"github.com/samarec1812/todo-app"
+	"github.com/samarec1812/todo-app/pkg/repository"
+	"github.com/golang-jwt/jwt"
+	"time"
+)
+
+const (
+	salt = "aiwnpfan3ptnp4tn13tmpm"
+	signingKey = "oawdnbod3534%%#t23nlfnfj"
+	tokenTTL =  12 * time.Hour
+)
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
+
+type AuthService struct {
+	repo repository.Authorization
+}
+
+func NewAuthService(repo repository.Authorization) *AuthService {
+	return &AuthService{repo: repo}
+}
+
+func (s *AuthService) CreateUser(user todo.User) (int, error) {
+	user.Password = generatePasswordHash(user.Password)
+	return s.repo.CreateUser(user)
+}
+
+func generatePasswordHash(password string) string {
+	hash := sha1.New()
+	hash.Write([]byte(password))
+
+	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	// get user from DB
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt: time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(signingKey))
+}
+
+func (s *AuthService) ParseToken(accessToken string) (int, error) {
+	token, err := jwt.ParseWithClaims(accessToken, &tokenClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("invalid signing method")
+		}
+		return []byte(signingKey), nil
+	})
+	if err != nil {
+		return 0, err
+	}
+	claims, ok := token.Claims.(*tokenClaims)
+	if !ok {
+		return 0, errors.New("token claims are not of type")
+	}
+
+	return claims.UserId, nil
+}
